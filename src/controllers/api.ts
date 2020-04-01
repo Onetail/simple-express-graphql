3719782cba6a68d6859d816058b3c976f5059352
onetail <paosong91@gmail.com>
2020-04-01 16:20:27 +0800

    delete not use code

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,8 +1,6 @@
 "use strict";
 
-import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
-import { UserDocument } from "../models/User";
 
 
 /**
@@ -19,15 +17,4 @@ export const getApi = (req: Request, res: Response) => {
  * GET /api/facebook
  * Facebook API example.
  */
-export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user as UserDocument;
-    const token = user.tokens.find((token: any) => token.kind === "facebook");
-    graph.setAccessToken(token.accessToken);
-    graph.get(`${user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-        if (err) { return next(err); }
-        res.render("api/facebook", {
-            title: "Facebook API",
-            profile: results
-        });
-    });
-};
+export const getFacebook = (req: Request, res: Response, next: NextFunction) => {};
